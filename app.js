70ba9c24d74ec47c86ef68764d98ba1fb90ec99e
Debi <75093017+debi-ps@users.noreply.github.com>
2021-03-29 14:41:00 +0300

    Created User model. Required packages: jsonwebtoken, cors, cookie-parser, bcrypt. Created routes for auth methods. Created auth controller.

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const PORT = config.get('app.port');
+
+app.use(
+	cors({
+		origin: 'http://localhost:3000',
+	})
+);
 
 mongoose.connect(config.get('db.url'), {
 	useNewUrlParser: true,
@@ -14,4 +22,34 @@ app.listen(PORT, () => {
 	console.log('Application is started on port ' + PORT);
 });
 
+// middlewares
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+// routes
 app.get('/ping', async (req, res) => res.send('pong'));
+
+// cookies
+app.get('/set-cookie', async (req, res) => {
+	// res.setHeader('Set-Cookie', 'newUser=true');
+
+	res.cookie('newUserOld', false, {
+		maxAge: 1000 * 60 * 60 * 24, // one day
+		secure: true,
+		httpOnly: true,
+	});
+
+	res.send("you've got a cookie");
+});
+
+app.get('/read-cookie', async (req, res) => {
+	const cookies = req.cookies;
+
+	console.log(cookies.newUser);
+
+	res.json(cookies);
+});
+
+// auth
+app.use(require('./routes/auth.routes'));
---
+++ controllers/auth.controller.js
@@ -0,0 +1,96 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcrypt');
+
+const User = require('../models/User');
+
+const maxAge = config.get('jwt.maxAge');
+
+const createToken = (id) => {
+	return jwt.sign({ id }, config.get('jwt.secret'), {
+		expiresIn: maxAge,
+	});
+};
+
+const setJwtCookie = (res, token) => {
+	res.cookie('jwt', token, {
+		httpOnly: true,
+		maxAge: maxAge * 1000,
+	});
+};
+
+// handle errors
+const handleError = (err) => {
+	const errors = {};
+
+	// duplicate error code
+	if (err.code === 11000) {
+		Object.keys(err.keyValue).forEach((field) => {
+			errors[field] = `Field ${field} is already in use.`;
+		});
+
+		return errors;
+	}
+
+	// validation errors
+	if (err.message.includes('User validation failed')) {
+		Object.values(err.errors).forEach(({ properties }) => {
+			if (['name', 'email', 'password'].includes(properties.path)) {
+				errors[properties.path] = properties.message;
+			}
+		});
+	}
+
+	return errors;
+};
+
+// Signup user
+const signup = async (req, res) => {
+	try {
+		const { name, email, password } = req.body;
+
+		const user = await User.create({ name, email, password, isAdmin: false });
+		const token = createToken(user._id);
+
+		res.cookie('jwt', token, {
+			httpOnly: true,
+			maxAge: maxAge * 1000,
+		});
+		res.status(201).json({ message: 'success' });
+	} catch (e) {
+		const errors = handleError(e);
+
+		res.status(400).json({ errors });
+	}
+};
+
+// Login user
+const login = async (req, res) => {
+	const { email, password } = req.body;
+
+	try {
+		const user = await User.login(email, password);
+
+		const token = createToken(user._id);
+
+		setJwtCookie(res, token);
+
+		res.json({ message: 'success' });
+	} catch (e) {
+		const errors = handleError(e);
+		res.status(400).json(errors);
+	}
+};
+
+// Logout user
+const logout = async (req, res) => {
+	try {
+		req.cookie();
+	} catch (e) {}
+};
+
+module.exports = {
+	signup,
+	login,
+	logout,
+};
